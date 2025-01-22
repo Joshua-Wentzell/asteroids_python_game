b7e0993bbeba38ef964af5c2aa88d6e119397fe4
jwentzel <38767750+SilentSword047@users.noreply.github.com>
2025-01-22 11:58:47 -0500

    added player logic to use circular collider but a visual triangle

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from constants import (
     ASTEROID_SPAWN_RATE,
     ASTEROID_MAX_RADIUS,
 )
+from player import Player
 
 
 def main():
@@ -14,12 +15,17 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
